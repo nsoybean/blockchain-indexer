0f74517518db09bff85979a107ad2f384c688bb6
nyangshawbin <shawbin.nyang@circles.asia>
2023-07-06 21:46:40 +0800

    test: delay jest test to wait for indexer test: test api e2e and assert response

--- src/indexer.e2e-spec.ts
+++ src/indexer.e2e-spec.ts
@@ -2,76 +2,48 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { IndexerModule } from './indexer.module';
+let app: INestApplication;
+import * as e2e_001_data from '../test/resources/e2e_001.json';
 
 describe('Indexer e2e', () => {
-  let app: INestApplication;
-
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [IndexerModule],
     }).compile();
 
     app = moduleFixture.createNestApplication(); // instantiate full nest runtime env
     await app.init();
+
+    console.log('🚀 waiting...');
+
+    await new Promise((r) => setTimeout(r, 5000));
   });
 
-  it('/client/blocks/count (GET)', async () => {
-    // return request(app.getHttpServer()) // call to request() hands us a wrapped HTTP Server
-    //   .get('/client/blocks/count')
-    //   .expect(200)
-    //   .expect('200');
-    expect(
-      await request(app.getHttpServer()).get('/client/blocks/count'),
-    ).toEqual('200');
+  it('should get 201 total blocks (raw)', () => {
+    return request(app.getHttpServer())
+      .get('/api/blocks/count')
+      .expect(200)
+      .expect('201');
+  });
+
+  it('should get two specific block. testID: e2e_001', () => {
+    return request(app.getHttpServer())
+      .get('/api/blocks?maxHeight=2')
+      .expect(200)
+      .expect(e2e_001_data);
+  });
+
+  it("should get 'FORKEDBLOCK' block at height 198. testID: e2e_002", async () => {
+    const response = await request(app.getHttpServer()).get(
+      '/api/block/height?height=198',
+    );
+    expect(response.body).toHaveProperty(
+      'hash',
+      'ef61aaf7f6f742ed825922b10ec504ee74cfcb9c71037706dd8a8FORKEDBLOCK',
+    );
   });
 
   afterAll(async () => {
     await app.close();
   });
 });
-
-// describe('/api/blocks', () => {
-//   it('should return block by height', async () => {
-//     throw Error('todo');
-//   });
-
-//   it('should return block by hash', async () => {
-//     throw Error('todo');
-//   });
-
-//   it('should list blocks', async () => {
-//     throw Error('todo');
-//   });
-
-//   describe('/api/blocks/{height}/transactions', () => {
-//     it('should list transactions for a given block', async () => {
-//       throw Error('todo');
-//     });
-//   });
-// });
-
-// describe('/api/addresses/{address}/transactions', () => {
-//   it('should list transactions for a given address', async () => {
-//     throw Error('todo');
-//   });
-// });
-
-// describe('Indexer durability', () => {
-//   it('should not lose indexed state if it goes down while indexing', () => {
-//     // More specifically, killing the service while it's indexing then bringing it back up again
-//     // should not result in re-indexing of the already indexed blocks
-//     throw Error('todo');
-//   });
-// });
-
-// describe('Block invalidation', () => {
-//   it('should not return blocks from invalidatedHeight onwards', async () => {
-//     // invalidating block 100 should invalidate block 100, 101, 102, ...
-//     throw Error('todo');
-//   });
-
-//   it('should not return transactions from invalidated blocks', async () => {
-//     // invalidating block 100 should invalidate all of its transactions
-//     throw Error('todo');
-//   });
-// });
